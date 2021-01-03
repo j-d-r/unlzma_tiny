aec480593c215465713854359f5f7e25433f0e9c
Julien Dusser <julien.dusser@free.fr>
2021-01-03 15:10:36 +0100

    Add const and restrict to enable more optimisations
    Signed-off-by: Julien Dusser <julien.dusser@free.fr>

--- unlzma_tiny.h
+++ unlzma_tiny.h
@@ -11,6 +11,6 @@
 /* Inflate LZMA in_ptr buffer into out_ptr buffer.
  * return decompressed size or (size_t)-1 if failed */
 
-size_t lzma_inflate(uint8_t *in_ptr, size_t in_size, uint8_t *out_ptr, size_t out_size);
+size_t lzma_inflate(const uint8_t *in_ptr, size_t in_size, uint8_t *restrict out_ptr, size_t out_size);
 
 #endif /* UNLZMA_TINY_H_ */
